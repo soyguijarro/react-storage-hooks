37fb905c554a907bee4a421bd49493eb97861899
Ramón Guijarro <hola@soyguijarro.com>
2019-03-23 12:05:03 +0100

    Split code into modules

---
+++ src/reducer.ts
@@ -0,0 +1,65 @@
+import { useEffect, useReducer } from 'react';
+import {
+  useStorageReader,
+  useStorageWriter,
+  useStorageListener,
+} from './storage';
+
+const INTERNAL_SET_ACTION_TYPE = Symbol('INTERNAL_SET_ACTION_TYPE');
+type InternalSetAction<S> = {
+  type: typeof INTERNAL_SET_ACTION_TYPE;
+  payload: S;
+};
+
+const createInternalSetAction = <S>(payload: S): InternalSetAction<S> => ({
+  type: INTERNAL_SET_ACTION_TYPE,
+  payload,
+});
+
+const isInternalSetAction = <S>(action: any): action is InternalSetAction<S> =>
+  action && action.type === INTERNAL_SET_ACTION_TYPE;
+
+const createStorageReducer = <R extends React.Reducer<any, any>, I>(
+  reducer: R
+) => (
+  prevState: React.ReducerState<R>,
+  action: I | InternalSetAction<React.ReducerState<R>>
+): React.ReducerState<R> =>
+  isInternalSetAction(action) ? action.payload : reducer(prevState, action);
+
+const createUseStorageReducer = (storage: Storage) => <
+  R extends React.Reducer<any, any>,
+  I
+>(
+  key: string,
+  reducer: R,
+  initializerArg: I & React.ReducerState<R>,
+  initializer?: (arg: I & React.ReducerState<R>) => React.ReducerState<R>
+): [
+  React.ReducerState<R>,
+  React.Dispatch<React.ReducerAction<R>>,
+  Error | undefined
+] => {
+  const storageReducer = createStorageReducer<R, I>(reducer);
+  const storageInitializerArg = useStorageReader(storage, key, initializerArg);
+  const [state, dispatch] = initializer
+    ? useReducer(storageReducer, storageInitializerArg, initializer)
+    : useReducer(storageReducer, storageInitializerArg);
+
+  const writeError = useStorageWriter(storage, key, state);
+  useStorageListener<React.ReducerState<R>>(key, newValue => {
+    dispatch(createInternalSetAction(newValue));
+  });
+
+  useEffect(() => {
+    dispatch(
+      createInternalSetAction(
+        initializer ? initializer(storageInitializerArg) : storageInitializerArg
+      )
+    );
+  }, [key]);
+
+  return [state, dispatch, writeError];
+};
+
+export default createUseStorageReducer;
---
+++ src/state.ts
@@ -0,0 +1,25 @@
+import { useEffect, useState } from 'react';
+import {
+  useStorageReader,
+  useStorageWriter,
+  useStorageListener,
+} from './storage';
+
+const createUseStorageState = (storage: Storage) => <S>(
+  key: string,
+  defaultState: S | (() => S)
+): [S, React.Dispatch<React.SetStateAction<S>>, Error | undefined] => {
+  const savedState = useStorageReader(storage, key, defaultState);
+
+  const [state, setState] = useState(savedState);
+  const writeError = useStorageWriter(storage, key, state);
+  useStorageListener<S>(key, setState);
+
+  useEffect(() => {
+    setState(savedState);
+  }, [key]);
+
+  return [state, setState, writeError];
+};
+
+export default createUseStorageState;
